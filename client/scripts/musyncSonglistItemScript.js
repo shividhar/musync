fe948c3b3b2740794729cf8ba8d65bd33422470b
spykill4 <adzikh@hotmail.com>
2015-10-06 18:29:02 +0000

    I dunno...stuff

---
+++ client/scripts/musyncSonglistItemScript.js
@@ -0,0 +1,21 @@
+Template.musyncSongListItem.helpers({
+    "apiCall" : function()
+    {
+        if(dataApiReady)
+        {
+            
+        }
+    }
+    , "vid" : function()
+    {
+        
+    }
+    , "songPosition" : function()
+    {
+        
+    }
+    , "songOriginPlaylist" : function()
+    {
+        
+    }
+});
