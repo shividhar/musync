13f9b96e03e73d830c9b0a19b63e368f640eb68e
Shivansh Dhar <shivanshdhar@gmail.com>
2015-10-06 18:15:35 +0000

    Added helpers and tested

--- client/scripts/musyncPlaylistScript.js
+++ client/scripts/musyncPlaylistScript.js
@@ -49,23 +49,28 @@
         searchResults : function()
         {
             return Session.get('results');
+        },
+        songList: function(){
+           return this.playlist.songList;
         }
     });
     Template.musyncPlaylist.events({
-        'click #searchButton, keydown #searchButton': function(e){
-             if(!dataApiReady){ document.getElementById('searchError').innerHTML = "Youtube Data API is not ready!"; return; }
-            var request = gapi.client.youtube.search.list({q: document.getElementById('searchField').value, maxResults: 10, part: 'snippet'});
-            request.execute(function(response)
-            {
-                var results = [];
-                for(var i in response.items)
+        'click #searchButton, keydown #searchField': function(e){
+            if(e.button == 0 || e.keyCode == 13){
+                 if(!dataApiReady){ document.getElementById('searchError').innerHTML = "Youtube Data API is not ready!"; return; }
+                var request = gapi.client.youtube.search.list({q: document.getElementById('searchField').value, maxResults: 10, part: 'snippet'});
+                request.execute(function(response)
                 {
-                    var item = response.items[i];
-                    results.push({ resultTitle: item.snippet.title, resultAuthor: item.snippet.channelTitle, resultThumb: item.snippet.thumbnails.default.url });
-                }
-                
-                Session.set('results', results);
-            });
+                    var results = [];
+                    for(var i in response.items)
+                    {
+                        var item = response.items[i];
+                        results.push({ resultTitle: item.snippet.title, resultAuthor: item.snippet.channelTitle, resultThumb: item.snippet.thumbnails.default.url });
+                    }
+                    
+                    Session.set('results', results);
+                });
+            }
         }
         ,'click #createPlaylist': function(e)
         {
