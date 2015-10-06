1e05b422ca5c71024a5f8b095f2b56c75fc0051d
Shivansh Dhar <shivanshdhar@gmail.com>
2015-10-06 17:27:57 +0000

    Fucked around

--- client/scripts/musyncPlaylistScript.js
+++ client/scripts/musyncPlaylistScript.js
@@ -61,71 +61,11 @@
           tag.src = 'https://www.youtube.com/iframe_api';
           document.head.appendChild(tag);
     }
-    Template.musyncPlaylist.helper({
-        playlistName: function()
-        {
-            //return 
-        });
     Template.musyncPlaylist.events({
         'click #searchButton, keydown #searchButton': function(e){
-             if(!dataApiReady){ document.getElementById('searchError').innerHTML = "Youtube Data API is not ready!"; return; }
-            var request = gapi.client.youtube.search.list({q: document.getElementById('searchField').value, maxResults: 10, part: 'snippet'});
-            request.execute(function(response)
-            {
-                var vRP = document.getElementById('videoResultParent');
-                while (vRP.firstChild)
-                {
-                    vRP.removeChild(vRP.firstChild);
-                }
-                for(var i in response.items)
-                {
-                    var item = response.items[i];
-                    console.log(item);
-                    var row = document.createElement('tr');
-                    vRP.appendChild(row);
-                    var cell = document.createElement('td');
-                    row.appendChild(cell);
-                    var vName = document.createElement('p');
-                    vName.innerHTML = item.snippet.title;
-                    cell.appendChild(vName);
-                    var vThumb = document.createElement('img');
-                    vThumb.src = item.snippet.thumbnails.default.url;
-                    cell.appendChild(vThumb);
-                    var vAdd = document.createElement('div');
-                    vAdd.style.cssText = 'width: 50px; height: 50px; background: rgb(230, 230, 230)';
-                    vAdd.className = "videoAddButton";
-                    vAdd.vId = item.id.videoId;
-                    cell.appendChild(vAdd);
-                    //var vAuthor = document.createElement('p');
-                    //vName.innerHTML = item.snippet.title;
-                    //cell.appendChild(vName);
-                }
-            });
+
         },
         'click div.videoAddButton': function(events){
             addSongToPlaylist(event.target.vId);
-        }
-        
-        , 'click #songlistAdd': function(e)
-        {
-            Meteor.call('addSongToPlaylist', { videoId: , playlistId: Playlists.findOne().playlistId});
-        }
-        
-        , 'click #songlistMoveUp': function(e)
-        {
-            if(e.target.pos !== 0)
-            {
-                Meteor.call("moveSongInPlaylist", { videoId: e.target.vid, playlistId: e.target.pid, songPosition: e.target.pos - 1 } );
-            }
-        }
-        
-        , 'click #songlistMoveDown': function(e)
-        {
-            Meteor.call("moveSongInPlaylist", { videoId: e.target.vid, playlistId: e.target.pid, songPosition: e.target.pos + 1 } );
-        }
-        
-        , 'click #songlistRemove': function(e)
-        {
-            Meteor.call("removeSongFromPlaylist", { videoId: e.target.vid, playlistId: e.target.pid });
         }
     })
