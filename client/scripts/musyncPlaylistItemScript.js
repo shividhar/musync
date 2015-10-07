7df92dc369df998cb8301c326652474d082a51f5
spykill4 <adzikh@hotmail.com>
2015-10-07 17:34:32 +0000

    Fixes x4

--- client/scripts/musyncPlaylistItemScript.js
+++ client/scripts/musyncPlaylistItemScript.js
@@ -6,22 +6,3 @@ Template.musyncPlaylistItem.helpers({
         return 'test1';
     }
 });
-Template.musyncPlaylistItem.events({
-    'click #songlistMoveUp': function(e)
-    {
-        if(e.target.pos !== 0)
-        {
-            Meteor.call("moveSongInPlaylist", { videoId: e.target.vid, playlistId: e.target.pid, songPosition: e.target.pos - 1 } );
-        }
-    }
-    
-    , 'click #songlistMoveDown': function(e)
-    {
-        Meteor.call("moveSongInPlaylist", { videoId: e.target.vid, playlistId: e.target.pid, songPosition: e.target.pos + 1 } );
-    }
-    
-    , 'click #songlistRemove': function(e)
-    {
-        Meteor.call("removeSongFromPlaylist", { videoId: e.target.vid, playlistId: e.target.pid });
-    }
-});
--- client/scripts/musyncSonglistItemScript.js
+++ client/scripts/musyncSonglistItemScript.js
@@ -49,3 +49,23 @@ Template.musyncSongListItem.helpers({
         return Session.get("songData" + this.videoId).author;
     }
 });
+
+Template.musyncSongListItem.events({
+    'click .songlistMoveUp': function(e)
+    {
+        if(e.target.pos !== 0)
+        {
+            Meteor.call("moveSongInPlaylist", { videoId: e.target.vid, playlistId: e.target.pid, songPosition: e.target.pos - 1 } );
+        }
+    }
+    
+    , 'click .songlistMoveDown': function(e)
+    {
+        Meteor.call("moveSongInPlaylist", { videoId: e.target.vid, playlistId: e.target.pid, songPosition: e.target.pos + 1 } );
+    }
+    
+    , 'click .songlistRemove': function(e)
+    {
+        Meteor.call("removeSongFromPlaylist", { videoId: e.target.vid, playlistId: e.target.pid });
+    }
+});
